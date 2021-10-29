fd864c90b2068f9356f94464c4a7cb735f700a98
SeemaMistry <seema.m.1996@gmail.com>
2021-10-29 19:43:06 -0400

    Add events to button for onclick, deafult props, and proptypes specification.

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -1,3 +1,23 @@
-export const Button = ({ text, color }) => {
-    return <button style={{ backgroundColor:color}} className='btn'>{text}</button>
+import PropTypes from 'prop-types'
+
+export const Button = ({ text, color, onClick }) => {
+    return (
+        <button 
+            style={{ backgroundColor:color}} 
+            className='btn'
+            onClick={onClick}
+        >
+            {text}
+        </button>
+        )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func
 }
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -2,10 +2,14 @@ import PropTypes from 'prop-types'
 import { Button } from './Button'
 
 export const Header = ({ title }) => {
+    const onClick = () => {
+        console.log("Clicked")
+    }
+
     return (
        <header className='header'>
            <h1>{title}</h1>
-           <Button text='Add' color='green'/>
+           <Button text='Add' color='green' onClick={onClick}/>
        </header>
     )
 }
