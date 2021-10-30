b515825a010ef9741c8ff6d0090960182cf960a5
SeemaMistry <seema.m.1996@gmail.com>
2021-10-29 20:32:00 -0400

    Create task component with html h3 text and html-p for date.

---
+++ react-task-tracker/src/components/Task.js
@@ -0,0 +1,8 @@
+export const Task = ({ task }) => {
+    return (
+        <div className='task'>
+            <h3 key={task.id}>{task.text}</h3>
+            <p>{task.day}</p>
+        </div>
+    )
+}
